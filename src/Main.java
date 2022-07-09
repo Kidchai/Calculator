4855d0250cae0d609d15de5093b8a103d06ea276
Kidchai <o.naibova@gmail.com>
2022-07-08 20:13:02 -0500

    added the definePriority() method
    - added field priority for Operator class
    - definePriority() defines and sets the priority for arithmetical operators from the list

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,89 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+    public static ArrayList<Element> list = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Please enter the problem and push \"Enter\".");
-            while (true) {
-                String input = "";
-                //long result = 0;
-                ArrayList<Element> list = new ArrayList<>();
-                Scanner scanner = new Scanner(System.in);
-                input = scanner.nextLine();
-                String buf = "";
-                for (int i = 0; i < input.length(); i++) {
-                    String element = Character.toString(input.charAt(i));
-                    if (input.charAt(i) == '+' ||
+        while (true) {
+            String input = "";
+            //long result = 0;
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            String buf = "";
+            for (int i = 0; i < input.length(); i++) {
+                String element = Character.toString(input.charAt(i));
+                if (input.charAt(i) == '+' ||
                         input.charAt(i) == '-' ||
                         input.charAt(i) == '*' ||
                         input.charAt(i) == '/') {
+                    list.add(new Number(buf));
+                    list.add(new Operator(element));
+                    buf = "";
+                } else {
+                    buf += element;
+                }
+
+                if (i == input.length() - 1) {
+                    if (!buf.equals("")) {
                         list.add(new Number(buf));
-                        list.add(new Operator(element));
-                        buf = "";
                     } else {
-                        buf += element;
-                    }
-
-                    if (i == input.length() - 1) {
-                        if (!buf.equals("")) {
-                            list.add(new Number(buf));
-                        } else {
-                            list.add(new Operator(element));
-                        }
+                        list.add(new Operator(element));
                     }
                 }
+            }
 
-                for (Element e: list) {
-                    System.out.print(e.makeString() + " ");
+            Main.definePriority();
+
+            for (Element e : list) {
+                System.out.print(e.makeString() + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void definePriority() {
+        for (Element e : list) {
+            if (e instanceof Operator) {
+                switch (e.makeString()) {
+                    case "*", "/" -> ((Operator) e).setPriority(1);
+                    case "-", "+" -> ((Operator) e).setPriority(0);
                 }
-                System.out.println("");
+            }
+        }
 
+        Set<Integer> allPriorities = new HashSet<>();
+        int priority = 1;
+        boolean isPriorityAsserted = false;
 
-//            if (input.contains("+")) {
-//                String[] array = input.split("\\+");
-//                result = Integer.parseInt(array[0]) + Integer.parseInt(array[1]);
-//            } else if (input.contains("-")) {
-//                String[] array = input.split("-");
-//                result = Integer.parseInt(array[0]) - Integer.parseInt(array[1]);
-//            } else if (input.contains("*")) {
-//                String[] array = input.split("\\*");
-//                result = (long) Integer.parseInt(array[0]) * Integer.parseInt(array[1]);
-//            } else if (input.contains("/")) {
-//                String[] array = input.split("/");
-//                result = Integer.parseInt(array[0]) / Integer.parseInt(array[1]);
-//            }
-//            System.out.println(result);
-
-            //1+2-4*4
+        for (Element e : list) {
+            if (e instanceof Operator && ((Operator) e).getPriority() == 1) {
+                isPriorityAsserted = false;
+                while (!isPriorityAsserted) {
+                    if (!allPriorities.contains(priority)) {
+                        ((Operator) e).setPriority(priority);
+                        allPriorities.add(priority);
+                        isPriorityAsserted = true;
+                    }
+                    priority++;
+                }
+            }
+        }
+        for (Element e : list) {
+            if (e instanceof Operator && ((Operator) e).getPriority() == 0) {
+                isPriorityAsserted = false;
+                while (!isPriorityAsserted) {
+                    if (!allPriorities.contains(priority)) {
+                        ((Operator) e).setPriority(priority);
+                        allPriorities.add(priority);
+                        isPriorityAsserted = true;
+                    }
+                    priority++;
+                }
+            }
         }
     }
 }
--- src/Operator.java
+++ src/Operator.java
@@ -4,6 +4,14 @@ public class Operator extends Element {
         this.operator = operator;
     }
 
+    private int priority;
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {return priority;}
+
     public String makeString() {
         return operator;
     }
