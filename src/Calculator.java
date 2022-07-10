bf983443dfef958b7a6829b9b123c9c9606a6fe1
Kidchai <o.naibova@gmail.com>
2022-07-10 18:43:35 -0500

    added Calculator class
    three methods (makeList, definePriority, calculate) moved from Main class to Calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+//класс принимает на ввод строку, возвращает число
+public class Calculator {
+    private static ArrayList<Element> list = new ArrayList<>();
+
+    void makeList(String input) {
+        String buf = "";
+        for (int i = 0; i < input.length(); i++) {
+            String element = Character.toString(input.charAt(i));
+            if (input.charAt(i) == '+' ||
+                    input.charAt(i) == '-' ||
+                    input.charAt(i) == '*' ||
+                    input.charAt(i) == '/') {
+                list.add(new Number(buf));
+                list.add(new Operator(element));
+                buf = "";
+            } else {
+                buf += element; //заменить на StringBuilder
+            }
+
+            if (i == input.length() - 1) {
+                if (!buf.equals("")) {
+                    list.add(new Number(buf));
+                } else {
+                    list.add(new Operator(element));
+                }
+            }
+        }
+    }
+
+    void definePriority() {
+        for (Element e : list) {
+            if (e instanceof Operator) {
+                switch (e.makeString()) {
+                    case "*", "/" -> ((Operator) e).setPriority(1);
+                    case "-", "+" -> ((Operator) e).setPriority(0);
+                }
+            }
+        }
+
+        Set<Integer> allPriorities = new HashSet<>();
+        int priority = 1;
+        boolean isPriorityAsserted;
+
+        for (Element e : list) {
+            if (e instanceof Operator && ((Operator) e).getPriority() == 1) {
+                isPriorityAsserted = false;
+                while (!isPriorityAsserted) {
+                    if (!allPriorities.contains(priority)) {
+                        ((Operator) e).setPriority(priority);
+                        allPriorities.add(priority);
+                        isPriorityAsserted = true;
+                    }
+                    priority++;
+                }
+            }
+        }
+        for (Element e : list) {
+            if (e instanceof Operator && ((Operator) e).getPriority() == 0) {
+                isPriorityAsserted = false;
+                while (!isPriorityAsserted) {
+                    if (!allPriorities.contains(priority)) {
+                        ((Operator) e).setPriority(priority);
+                        allPriorities.add(priority);
+                        isPriorityAsserted = true;
+                    }
+                    priority++;
+                }
+            }
+        }
+    }
+
+    int calculate() {
+        int priorityCounter = 1;
+        while (list.size() != 1) {
+            for (int i = 0; i < list.size(); i++) {
+                Element element = list.get(i);
+                if (element instanceof Operator && ((Operator) element).getPriority() == priorityCounter) {
+                    Element leftElement = list.get(i - 1);
+                    Element rightElement = list.get(i + 1);
+                    int leftNumber = Integer.parseInt((leftElement).makeString());
+                    int rightNumber = Integer.parseInt((rightElement).makeString());
+                    int result = 0;
+                    switch (element.makeString()) {
+                        case "*" -> result = leftNumber * rightNumber;
+                        case "/" -> result = leftNumber / rightNumber;
+                        case "+" -> result = leftNumber + rightNumber;
+                        case "-" -> result = leftNumber - rightNumber;
+                    }
+                    list.set(list.indexOf(leftElement), new Number(String.valueOf(result)));
+                    list.remove(i + 1);
+                    list.remove(i);
+                    priorityCounter++;
+                    break;
+                }
+            }
+        }
+        return Integer.parseInt(list.get(0).makeString());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,109 +4,17 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    public static ArrayList<Element> list = new ArrayList<>();
-
     public static void main(String[] args) {
         System.out.println("Please enter the problem and push \"Enter\".");
         while (true) {
-            String input = "";
             Scanner scanner = new Scanner(System.in); //заменить
-            input = scanner.nextLine();
-            String buf = "";
-            for (int i = 0; i < input.length(); i++) {
-                String element = Character.toString(input.charAt(i));
-                if (input.charAt(i) == '+' ||
-                        input.charAt(i) == '-' ||
-                        input.charAt(i) == '*' ||
-                        input.charAt(i) == '/') {
-                    list.add(new Number(buf));
-                    list.add(new Operator(element));
-                    buf = "";
-                } else {
-                    buf += element; //заменить на StringBuilder
-                }
+            String input = scanner.nextLine();
 
-                if (i == input.length() - 1) {
-                    if (!buf.equals("")) {
-                        list.add(new Number(buf));
-                    } else {
-                        list.add(new Operator(element));
-                    }
-                }
-            }
+            Calculator calculator = new Calculator();
+            calculator.makeList(input);
+            calculator.definePriority();
 
-            Main.definePriority();
-            System.out.println(Main.calculate());
+            System.out.println(calculator.calculate());
         }
-    }
-
-    private static void definePriority() {
-        for (Element e : list) {
-            if (e instanceof Operator) {
-                switch (e.makeString()) {
-                    case "*", "/" -> ((Operator) e).setPriority(1);
-                    case "-", "+" -> ((Operator) e).setPriority(0);
-                }
-            }
-        }
-
-        Set<Integer> allPriorities = new HashSet<>();
-        int priority = 1;
-        boolean isPriorityAsserted;
-
-        for (Element e : list) {
-            if (e instanceof Operator && ((Operator) e).getPriority() == 1) {
-                isPriorityAsserted = false;
-                while (!isPriorityAsserted) {
-                    if (!allPriorities.contains(priority)) {
-                        ((Operator) e).setPriority(priority);
-                        allPriorities.add(priority);
-                        isPriorityAsserted = true;
-                    }
-                    priority++;
-                }
-            }
-        }
-        for (Element e : list) {
-            if (e instanceof Operator && ((Operator) e).getPriority() == 0) {
-                isPriorityAsserted = false;
-                while (!isPriorityAsserted) {
-                    if (!allPriorities.contains(priority)) {
-                        ((Operator) e).setPriority(priority);
-                        allPriorities.add(priority);
-                        isPriorityAsserted = true;
-                    }
-                    priority++;
-                }
-            }
-        }
-    }
-
-    private static int calculate() {
-        int priorityCounter = 1;
-        while (list.size() != 1) {
-            for (int i = 0; i < list.size(); i++) {
-                Element element = list.get(i);
-                if (element instanceof Operator && ((Operator) element).getPriority() == priorityCounter) {
-                    Element leftElement = list.get(i - 1);
-                    Element rightElement = list.get(i + 1);
-                    int leftNumber = Integer.parseInt((leftElement).makeString());
-                    int rightNumber = Integer.parseInt((rightElement).makeString());
-                    int result = 0;
-                    switch (element.makeString()) {
-                        case "*" -> result = leftNumber * rightNumber;
-                        case "/" -> result = leftNumber / rightNumber;
-                        case "+" -> result = leftNumber + rightNumber;
-                        case "-" -> result = leftNumber - rightNumber;
-                    }
-                    list.set(list.indexOf(leftElement), new Number(String.valueOf(result)));
-                    list.remove(i + 1);
-                    list.remove(i);
-                    priorityCounter++;
-                    break;
-                }
-            }
-        }
-        return Integer.parseInt(list.get(0).makeString());
     }
 }
